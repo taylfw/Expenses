1c186510a0bd08eac63c5d6b077688ed3fa5d591
taylfw88 <frank.w.taylor88@gmail.com>
2021-10-20 14:18:06 -0400

    React Course Porject from Udemy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Expenses from "./components/Expenses/Expenses";
 
+import NewExpense from "./components/NewExpense/NewExpense";
+
 function App() {
   const expenses = [
     {
@@ -23,9 +25,14 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
